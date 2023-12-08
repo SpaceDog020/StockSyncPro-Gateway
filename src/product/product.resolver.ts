98fae2a2d565a8e7420b7f492450dc627ee24ab5
Jean Lopez <jeanlopezcortes@gmail.com>
2023-12-08 16:32:54 -0300

    update and delete product

--- src/product/product.resolver.ts
+++ src/product/product.resolver.ts
@@ -2,7 +2,7 @@ import { Inject, OnModuleInit } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable, map } from 'rxjs';
-import { AddProductRequest, AddProductResponse, GetAllProductsResponse, ProductServiceClient, getProductByIdRequest, getProductByIdResponse } from './product.pb';
+import { AddProductRequest, AddProductResponse, DeleteProductByIdRequest, DeleteProductByIdResponse, GetAllProductsResponse, ProductServiceClient, UpdateProductRequest, UpdateProductResponse, getProductByIdRequest, getProductByIdResponse } from './product.pb';
 
 @Resolver('Product')
 export class ProductResolver implements OnModuleInit {
@@ -31,4 +31,17 @@ export class ProductResolver implements OnModuleInit {
         console.log('[.] addProduct');
         return this.productService.addProduct(input);
     }
+
+    @Mutation('deleteProduct')
+    deleteProduct(@Args('input') input: DeleteProductByIdRequest): Observable<DeleteProductByIdResponse> {
+        console.log('[.] deleteProduct');
+        return this.productService.deleteProductById(input);
+    }    
+
+    @Mutation('updateProduct')
+    updateProduct(@Args('input') input: UpdateProductRequest): Observable<UpdateProductResponse> {
+        console.log('[.] updateProduct');
+        return this.productService.updateProduct(input);
+    }    
+
 }
