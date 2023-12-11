25b7dd8cc992eb0c393862774e9b3f9df0f1a700
Sebastián Astudillo <sebastianastudillorojas@gmail.com>
2023-12-10 22:19:31 -0300

    add user resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,8 +1,8 @@
-import { Inject, OnModuleInit } from '@nestjs/common';
+import { Body, Inject, OnModuleInit } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable, map } from 'rxjs';
-import { UserServiceClient } from './user.pb';
+import { CredentialsRequest, LoginUserResponse, RegisterUserResponse, UserServiceClient } from './user.pb';
 
 @Resolver('User')
 export class UserResolver implements OnModuleInit {
@@ -13,5 +13,15 @@ export class UserResolver implements OnModuleInit {
         this.userService = this.client.getService<UserServiceClient>('UserService');
     }
 
+    
+    @Mutation('register')
+    register(@Args('request') request: CredentialsRequest): Observable<RegisterUserResponse> {
+      return this.userService.register(request);
+    }
+
+    @Query('login')
+    login(@Args('request') request: CredentialsRequest): Observable<LoginUserResponse> {  
+      return this.userService.login(request);
+    }
     
 }
